30c1c35305e1b4c316df8c0fd2125cd5d1572f45
LuisFOsG <luisfernandogarciaosorio@gmail.com>
2020-11-25 14:48:48 -0500

    Version 1.0 Lista

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 cargar();
 
+ctx = [];
+
+defaultGrafica = "bar";
+tipoGrafica = [
+    "bar",
+    "bar",
+    "bar"
+]
+
 function load(){
     console.log(pregunta);
     console.log(respuesta);
 
-    graficar("barr", "bar", pregunta[0], respuesta[0]);
+    for(let i=0; i<pregunta.length; i++){
+        let identificador = "num"+i;
+        ctx.push(identificador);
+    }
+
+    for(let i=0; i<ctx.length; i++){
+        crearElemento(ctx[i]);
+    }
+
+    for(let i=0; i<pregunta.length; i++){
+        if(tipoGrafica[i]===undefined){
+            graficar(ctx[i], defaultGrafica, pregunta[i], respuesta[i]);
+        }else{
+            graficar(ctx[i], tipoGrafica[i], pregunta[i], respuesta[i]);
+        }
+    }
+}
+
+function crearElemento(nomid){
+    const app = document.getElementById("app");
+    let newElement = document.createElement("div");
+    let newCanvas = document.createElement("canvas");
+
+    newCanvas.id = nomid;
+    newElement.setAttribute("class", "col s6");
+
+    newElement.appendChild(newCanvas);
+    app.appendChild(newElement);
 }
